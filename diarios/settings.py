717a84882d1cee542e41bf38537820f434b314fe
Thiago Curvelo <tcurvelo@gmail.com>
2014-04-25 23:39:18 -0300

    initial structure generated by scrapy tool

---
+++ diarios/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for diarios project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'diarios'
+
+SPIDER_MODULES = ['diarios.spiders']
+NEWSPIDER_MODULE = 'diarios.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'diarios (+http://www.yourdomain.com)'
