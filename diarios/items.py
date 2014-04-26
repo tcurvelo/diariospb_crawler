afec338ff569fb98bee71d8b0f77e2cd0354dcd5
Thiago Curvelo <tcurvelo@gmail.com>
2014-04-26 01:52:23 -0300

    add a first version for a CrawlSpider
    - Add a simple Item (title, url)
    - Add a simple rule for link extraction
    - Add a parse_item() that returns Item or a request for one

--- diarios/items.py
+++ diarios/items.py
@@ -5,7 +5,7 @@
 
 from scrapy.item import Item, Field
 
+
 class DiariosItem(Item):
-    # define the fields for your item here like:
-    # name = Field()
-    pass
+    title = Field()
+    url = Field()
--- diarios/spiders/diario_pb.py
+++ diarios/spiders/diario_pb.py
@@ -1,22 +1,33 @@
-from scrapy.selector import Selector
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
-from scrapy.contrib.spiders import CrawlSpider, Rule
 from diarios.items import DiariosItem
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.contrib.spiders import CrawlSpider
+from scrapy.contrib.spiders import Rule
+from scrapy.http import Request
+from scrapy.selector import Selector
 
 
 class DiarioPbSpider(CrawlSpider):
     name = 'diario-pb'
     allowed_domains = ['paraiba.pb.gov.br']
-    start_urls = ['http://www.paraiba.pb.gov.br/']
+    start_urls = ['http://www.paraiba.pb.gov.br/diario-oficial']
 
     rules = (
-        Rule(SgmlLinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(
+            SgmlLinkExtractor(restrict_xpaths='//*[@id="busca_result"]/*/h3'),
+            callback='parse_item'
+        ),
     )
 
     def parse_item(self, response):
         sel = Selector(response)
-        i = DiariosItem()
-        #i['domain_id'] = sel.xpath('//input[@id="sid"]/@value').extract()
-        #i['name'] = sel.xpath('//div[@id="name"]').extract()
-        #i['description'] = sel.xpath('//div[@id="description"]').extract()
-        return i
+        links = sel.xpath('//*[@id="conteudo-interna"]/*/a')
+
+        for link in links:
+            url = link.xpath('@href').extract()[0]
+            if url.endswith(".pdf"):
+                yield DiariosItem(
+                    title=link.xpath('text()').extract(),
+                    url=url
+                )
+            else:
+                yield Request(url, callback=self.parse_item)
